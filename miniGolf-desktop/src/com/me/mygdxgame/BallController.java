364fa923f9b41c0d4ba070f9d7a434148d306ed3
JuanAstor <juan.astorgaalsina@uqconnect.edu.au>
2013-09-08 19:07:25 +1000

    Corner bugs almost fixed. Almost works %100

--- miniGolf-desktop/src/com/me/mygdxgame/BallController.java
+++ miniGolf-desktop/src/com/me/mygdxgame/BallController.java
@@ -9,66 +9,77 @@ public class BallController {
 	private Ball ball;
 	private Array<Block1> collisionBlocks;
 	private Array<Block1> openBlocks;
+	private Array<Block1> cornerBlocks;
 	private Boolean collision = false;
-	
-	public BallController(World world){
+
+	public BallController(World world) {
 		this.world = world;
-		this.ball = world.getBall();	
+		this.ball = world.getBall();
 		this.collisionBlocks = world.getWallBlocks();
 		this.collisionBlocks.addAll(world.getHoleBlock());
 		this.openBlocks = world.getGroundBlocks();
+		this.cornerBlocks = world.getCornerBlocks();
 	}
-	
-	
-	public void update(){
+
+	public void update() {
 		collisionDetect();
 	}
-	
-	public void collisionDetect(){
+
+	public void collisionDetect() {
 		Polygon ballBounds = ball.getBounds();
-		for (Block1 block: collisionBlocks){
+		for (Block1 block : collisionBlocks) {
 			Block1.FacingDir blockDir = block.getDir();
 			Block1.BlockType blockType = block.getType();
-			if(Intersector.overlapConvexPolygons(ballBounds, block.getBounds())){
-				
-				if(collision == false){
-				
-				
-					if(blockType == Block1.BlockType.WALL){
-						if((blockDir == Block1.FacingDir.EAST) ||
-								(blockDir == Block1.FacingDir.WEST)){ 
-							ball.bounceX = ball.bounceX == true? false:true;
+			Boolean boundColl = Intersector.overlapConvexPolygons(ballBounds,
+					block.getBounds());
+			if (boundColl) {
+
+				if (collision == false) {
+
+					if (blockType == Block1.BlockType.WALL) {
+						if ((blockDir == Block1.FacingDir.EAST)
+								|| (blockDir == Block1.FacingDir.WEST)) {
+							ball.bounceX = ball.bounceX == true ? false : true;
 						} else {
-							ball.bounceY = ball.bounceY == true? false:true;
+							ball.bounceY = ball.bounceY == true ? false : true;
 						}
 						collision = true;
 					}
-				
-					else if(blockType == Block1.BlockType.CORNER){
-						ball.bounceX = ball.bounceX == true? false:true;
-						ball.bounceY = ball.bounceY == true? false:true;
-						collision = true;
-					}
-				
-					else if(blockType == Block1.BlockType.HOLE){
-						if(ball.inHole == false){
+					else if (blockType == Block1.BlockType.HOLE) {
+						if (ball.inHole == false) {
 							System.out.println("YOU WIN!!!! (Not really)");
 							ball.inHole = true;
 							collision = true;
 						}
 					}
-					
+
 				}
-				
-			}			
+
+			}
 		}
-		for (Block1 groundBlock: openBlocks){
-			if(Intersector.overlapConvexPolygons(ballBounds, groundBlock.getBounds())){
-				if(collision == true){
+		
+		for (Block1 cornerBlock : cornerBlocks){
+			Boolean boundColl = Intersector.overlapConvexPolygons(ballBounds,
+					cornerBlock.getBounds());
+			Boolean boundTooColl = Intersector.overlapConvexPolygons(
+					ballBounds, cornerBlock.getBoundsToo());
+			if(boundColl || boundTooColl){
+				if (!collision){
+					if (boundColl) ball.bounceX = ball.bounceX == true ? false : true;
+					if (boundTooColl) ball.bounceY = ball.bounceY == true ? false : true;
+					collision = true;
+				}
+			}
+		}
+		
+		for (Block1 groundBlock : openBlocks) {
+			if (Intersector.overlapConvexPolygons(ballBounds,
+					groundBlock.getBounds())) {
+				if (collision == true) {
 					collision = false;
 				}
 			}
 		}
 	}
-	
-}	
+
+}
